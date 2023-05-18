68a19e2c6b6ca1fa8caa371396bc1381aa624022
shmatochok-maselka <iryna.kopchak39@gmail.com>
2023-05-19 00:38:51 +0300

    Init laravel project

---
+++ config/panel.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'date_format'         => 'Y-m-d',
+    'time_format'         => 'H:i:s',
+    'primary_language'    => 'uk',
+    'available_languages' => [
+        'uk' => 'English',
+    ],
+];
